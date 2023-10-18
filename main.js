41f875d2582c668783a8e34b16444e62ab6e4fcc
Jeremias Cuello <cuellojeremiasnatanael@gmail.com>
2023-10-18 13:44:17 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const generateNumbers = size => {
+  let numbers = [];
+
+  for (let i = 0; i < size; i++) {
+    let numRan = Math.round(Math.random() * 10);
+    while (numRan == 10) {
+      numRan = Math.round(Math.random() * 10);
+    }
+    numbers.push(numRan);
+  }
+
+  return numbers;
+}
+
+/**
+ *
+ * @param {number[]} numbers
+ */
+const procesar = numbers => {
+  let result = [];
+  let counter = 1;
+  let dsp;
+  numbers = numbers.sort();
+  let [ant] = numbers;
+  console.log(numbers.toString());
+
+  for (let i = 1; i < numbers.length; i++) {
+    dsp = numbers[i];
+    console.log(ant, dsp);
+    if(ant === dsp){
+      counter++;
+    } else{
+      result.push({num: ant, repeat: counter});
+      counter = 1;
+      ant = dsp;
+    }
+  }
+  result.push({num: ant, repeat: counter});
+
+  return result;
+}
